80fc8624eeeea9701af8cfbba4e848ee5d1c9b3e
mrsns2503 <121515204+mrsns2503@users.noreply.github.com>
2023-02-26 20:02:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/mrsns2503/SNS_PBOT.git /SNS_PBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /SNS_PBOT
 fi
-cd /EvaMaria
+cd /SNS_PBOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
